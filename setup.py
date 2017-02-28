0bb9a5039151166c837a5722e654833b96c51e85
Patrick Michaud <pmichaud@uw.edu>
2017-02-28 08:56:57 -0800

    Adding enough to install somewhere useful

---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/uw-restclients-hfs>`_.
+"""
+
+# The VERSION file is created by travis-ci, based on the tag name
+version_path = 'uw_hfs/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+url = "https://github.com/uw-it-aca/uw-restclients-hfs"
+setup(
+    name='UW-RestClients-HFS',
+    version=VERSION,
+    packages=['uw_hfs'],
+    author="UW-IT AXDD",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=['commonconf',
+                      'urllib3'],
+    license='Apache License, Version 2.0',
+    description=('A library for connecting to HFS services at the University '
+                 'of Washington'),
+    long_description=README,
+    url=url,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
+
+
