2be84576ceeaa90aef59d275ce8a267bcd72a5fb
Fang Lin <fanglin@uw.edu>
2018-09-06 16:16:46 -0700

    python 3.6 only

--- uw_hfs/__init__.py
+++ uw_hfs/__init__.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 def get_resource(url):
     response = DAO.getURL(url, {})
-    logger.info("%s ==status==> %s" % (url, response.status))
+    logger.debug("%s ==status==> %s", url, response.status)
 
     if response.status != 200:
         raise DataFailureException(url, response.status, response.data)
@@ -25,21 +25,21 @@ def get_resource(url):
     # 'Bug' with lib API causing requests with no/invalid user to return a 200
     if INVALID_PARAM_MSG in response.data:
         json_data = json.loads(response.data)
-        raise DataFailureException(url, 400, json_data["Message"])
+        raise DataFailureException(url, 400, str(json_data["Message"]))
 
     if INVALID_ID_MSG in response.data:
         json_data = json.loads(response.data)
-        raise DataFailureException(url, 404, json_data["Message"])
+        raise DataFailureException(url, 404, str(json_data["Message"]))
 
     if ERROR_MSG in response.data:
         json_data = json.loads(response.data)
-        raise DataFailureException(url, 500, json_data["Message"])
+        raise DataFailureException(url, 500, str(json_data["Message"]))
 
     try:
-        logger.debug("%s ==data==> %s" % (url, response.data.decode('utf-8')))
+        logger.debug("%s ==data==> %s", url, response.data.decode('utf-8'))
     except Exception as ex:
         # MUWM-2414
-        logger.debug("%s ==data==> Data not decodable to log file: %s" % (url,
-                                                                          ex))
+        logger.debug(
+            "%s ==data==> Data not decodable to log file: %s", url, str(ex))
 
     return response.data
