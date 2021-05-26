7315f419f02baf64f1d79e9db6b25a43460f2eb1
Jim Laney <jlaney@uw.edu>
2021-05-26 08:14:39 -0700

    move to github-actions

--- uw_hfs/util.py
+++ uw_hfs/util.py
@@ -1,3 +1,6 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from datetime import datetime, timedelta
 from restclients_core.util.decorators import use_mock
 from uw_hfs.dao import Hfs_DAO
