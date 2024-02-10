0013899d00875a086ee97d7f21d31869cf8b3c30
Fang Lin <fanglin@uw.edu>
2024-02-09 21:10:26 -0800

    suport python 3.10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name='UW-RestClients-HFS',
     version=VERSION,
     packages=['uw_hfs'],
-    author="UW-IT AXDD",
+    author="UW-IT T&LS",
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
@@ -37,6 +37,5 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
     ],
 )
